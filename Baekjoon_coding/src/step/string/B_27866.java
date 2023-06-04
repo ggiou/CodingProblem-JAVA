fd9eea51cda528280bdb3aea3214bd9003fe7e10
Majiwoo <majiou4420@naver.com>
2023-06-04 12:41:07 +0900

    add B_2743

--- Baekjoon_coding/src/step/string/B_27866.java
+++ Baekjoon_coding/src/step/string/B_27866.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class B_27866 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //입력이 적어도, 버퍼가 scanner보다 훨씬 효율이 좋음..
 		String en = br.readLine();
 		int i = Integer.parseInt(br.readLine())-1; //글자의 index가 0부터 시작인데 i번째 글자니 -1을 해줘야함
 		System.out.print(en.charAt(i));
