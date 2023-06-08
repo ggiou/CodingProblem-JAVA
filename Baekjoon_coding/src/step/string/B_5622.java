6fc39b55932e3a8452644d8420a52334fccf1fa7
Majiwoo <majiou4420@naver.com>
2023-06-08 20:12:10 +0900

    add Backjoon 5622

--- Baekjoon_coding/src/step/string/B_5622.java
+++ Baekjoon_coding/src/step/string/B_5622.java
@@ -10,7 +10,7 @@ public class B_5622 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<String> dial = Arrays.asList("ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ", "0");
+		List<String> dial = Arrays.asList("ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ");
 		//각 인덱스 값으로 최소 시간을 구하기 위해, 각 숫자칸의 문자에대해 리스트에 저장해둠 -> 배열리스트 전환 = arrays.asList
 		String[] word = br.readLine().split(""); //단어 하나당 처음으로 돌아가야하니, ""을 기준으로 분리
 		int time = 0;
@@ -30,5 +30,6 @@ public class B_5622 {
 	}
 }
 //[다이얼] : 입력받은 문자를 규칙에 맞게 숫자를 더해 출력하는 문제 (다이얼 문자마다 걸리는 시간이 다름, 총 소요시간 출력)
-//우선 다이얼의 문자를 리스트에 담은 후, 입력받은 문자열의 문자를 각각 리스트에서 포함되있는지 확인해 그 포함된 리스트의 인덱스+3이 소요시간이니, 이 소요시간을 모두 더하면서 문제를 풀었다.
+//우선 다이얼의 문자를 리스트에 담은 후, 입력받은 문자열의 문자를 각각 리스트에서 포함되있는지 확인해 
+//포함되있다면, 그 번호를 부르는 소요시간을 구해야하고, 소요시간은 리스트의 인덱스+3이니, 각 문자의 번호를 부르는 소요시간을 모두 더하는 방식으로 문제를 풀었다.
 
