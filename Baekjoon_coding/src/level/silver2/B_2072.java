6e265ada32d7af8dafea77c1826a1e445c5b2a7a
ggiou <majiou4420@naver.com>
2023-09-18 16:35:36 +0900

    백준 문제 풀거 미리 commitit

---
+++ Baekjoon_coding/src/level/silver2/B_2072.java
@@ -0,0 +1,17 @@
+package level.silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2072 {
+	static int[][] blackCheckerboard;
+	static int[][] whiteCheckerboard;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
